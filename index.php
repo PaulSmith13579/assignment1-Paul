07aded510b8aefda710dc023ace6f1450135b17d
0717097 <0717097@scholsnet.act.edu.au>
2021-08-06 11:56:31 +1000

    added new php files, as well as updating registration file for all updated lines of code

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php include "template.php"; ?>
+<?php include 'login.php'; ?>
 <title>Homepage</title>
+<h1 class='text-primary'>Welcome to our shopfront</h1>
+<?php if (!isset($_SESSION["username"])) : ?>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" required="required"/>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" required="required"/>
+        </div>
 
-Home Page
+        <center>
+            <button name="login" class="btn btn-primary"><span
+                        class="glyphicon glyphicon-log-in"></span> Login
+            </button>
+        </center>
+    </form>
+<?php endif; ?>
+</body>
+</html>
