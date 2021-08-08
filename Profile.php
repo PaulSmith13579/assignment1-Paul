bc0b19bed33b5d2e60b0b78174e5916b247d0c2c
0717097 <0717097@scholsnet.act.edu.au>
2021-08-09 09:42:46 +1000

    Updated profile page to display most of the information

--- Profile.php
+++ Profile.php
@@ -1,4 +1,23 @@
 <?php include "template.php"; ?>
-<title>User Profile</title>
+    <title>User Profile</title>
 
-<h1 class='text-primary'>Profile Page</h1>
+    <h1 class='text-primary'>Profile Page</h1>
+<?php
+
+echo $_SESSION["user_id"];
+echo "<br>";
+echo $_SESSION["username"];
+echo "<br>";
+echo $_SESSION["Password"];
+echo "<br>";
+echo $_SESSION["name"];
+echo "<br>";
+echo $_SESSION["profilePic"];
+echo "<br>";
+echo $_SESSION['level'];
+echo "<br>";
+
+
+
+
+?>
